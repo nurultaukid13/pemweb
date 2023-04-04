765dfdb535e7594c6942cbf1b605e78910c24985
nurultaukid13 <nurultaukid17@gmail.com>
2023-04-04 17:09:23 +0700

    uts

---
+++ transupn/main/koneksi.php
@@ -0,0 +1,17 @@
+<?php
+function koneksi_db (){
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db = 'transupn';
+    $conn = mysqli_connect($host, $user, $pass);
+    
+    if(! $conn){
+        die('koneksi gagal: '. mysqli_error($conn));
+    }
+    
+    mysqli_select_db($conn,$db);
+    
+    return $conn;
+}
+?>
