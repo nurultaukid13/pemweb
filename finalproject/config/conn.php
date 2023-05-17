5b7df216fcdcf8e43b6af038996faa0f20b4d028
nurultaukid13 <nurultaukid17@gmail.com>
2023-05-17 20:10:16 +0700

    coba

--- finalproject/config/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $hostname   = "localhost";
-    $username   = "root";
-    $password   = "";
-    $database   = "inventaris";
-
-    $con = mysqli_connect($hostname, $username, $password, $database) or die (mysqli_error($con));
-?>
